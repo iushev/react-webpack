81cec567cbe2e311ecb43caf61db0d03f0ee7a3b
Ivaylo Ushev <iushev@yahoo.com>
2017-10-25 14:05:50 +0300

    Add webpack-dev-server

---
+++ src/client/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return <p>Hello React, Webpack!</p>;
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'build');
-var APP_DIR = path.resolve(__dirname, 'src');
+var APP_DIR = path.resolve(__dirname, 'src/client');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+    entry: APP_DIR + '/index.js',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -16,10 +17,25 @@ var config = {
                 test: /\.jsx?/,
                 include: APP_DIR,
                 loader: 'babel-loader'
-            }
+            },
+            {
+                test: /\.css/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            },
         ]
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
 };
 
 module.exports = config;
